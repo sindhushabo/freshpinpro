2caa10ecbfe56e1886cb5df34e08d20562ac6b89
sindhushabo <srisindhusha10@gmail.com>
2016-05-17 03:11:05 -0400

    still working2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,14 +78,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # For Image Hosting on Amazon S3
-  config.paperclip_defaults = {
-      :storage => :s3,
-      :s3_credentials => {
-          :bucket => ENV['AWS_BUCKET'],
-          :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-          :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-      }
-  }
+
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
